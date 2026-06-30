30c17b64a4f800d84445fae0960b5c06b17bc301
rabie merouanie <merouani1706@gmail.com>
2026-06-30 02:37:17 +0100

    add connection function to establish a connection to sqlite and return as dict

--- src/database.py
+++ src/database.py
@@ -3,3 +3,9 @@ import json
 from datetime import datetime
 
 DB_NAME = "netguard.db"
+def get_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
